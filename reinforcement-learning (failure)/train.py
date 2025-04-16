5ba7dc132bf6962f0ee13f94ff6a09bf655ea173
Ojima Ryoji <ryoji.ojima@biprogy.com>
2025-04-17 08:58:23 +0900

    リファクタリング。

--- reinforcement-learning (failure)/train.py
+++ reinforcement-learning (failure)/train.py
@@ -1,6 +1,7 @@
 import os
 
 from pyarrow.fs import LocalFileSystem
+from random import Random
 from ray import init
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
@@ -10,6 +11,8 @@ from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from ray.tune.registry import register_env
 from env import PhysicalQuoridorEnv_
 
+
+rng = Random(1234)
 
 init()
 
@@ -22,20 +25,26 @@ config = (
     PPOConfig()
     .environment("physical_quoridor")
     .multi_agent(
-        policies={"policy_0"},
-        policy_mapping_fn=lambda agent, episode, **kwargs: "policy_0"
+        policy_mapping_fn=lambda agent, episode, **kwargs: rng.choice("policy-0", "policy-1"),
+        algorithm_config_overrides_per_module={
+            "policy-0": PPOConfig.overrides(gamme=0.995),
+            "policy-1": PPOConfig.overrides(gamme=0.9)
+        }
     )
     .rl_module(rl_module_spec=MultiRLModuleSpec(rl_module_specs={
-        "policy_0": RLModuleSpec(
+        "policy-0": RLModuleSpec(
+            model_config=DefaultModelConfig(
+                fcnet_hiddens=[512, 512, 512, 512],
+                fcnet_activation="relu"
+            )
+        ),
+        "policy-1": RLModuleSpec(
             model_config=DefaultModelConfig(
                 fcnet_hiddens=[512, 512, 512, 512],
                 fcnet_activation="relu"
             )
         )
     }))
-    .training(
-        gamma=0.995
-    )
     .env_runners(num_env_runners=8)
 )
 
@@ -44,7 +53,7 @@ algo = config.build_algo()
 for i in range(1_000_000):
     algo.train()
 
-    if (i + 1) % 10 == 0:
+    if (i + 1) % 100 == 0:
         checkpoint_path = f"./checkpoints/{i + 1:06}"
 
         os.makedirs(checkpoint_path, exist_ok=True)
