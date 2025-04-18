5c59bf721ab7ca549bcd18d5076d49e0a4be8c67
Ojima Ryoji <ryoji.ojima@biprogy.com>
2025-04-18 11:21:38 +0900

    リプレイ機能を追加。

--- game/play.py
+++ game/play.py
@@ -5,6 +5,7 @@ import sys
 
 from argparse import ArgumentParser
 from physical_quoridor import PhysicalQuoridorEnv
+from random import Random
 from time import sleep, perf_counter
 
 
@@ -50,13 +51,19 @@ async def create_player(command, log_file):
 
 
 class Game:
-    def __init__(self, player_0, player_1, seed, wait):
+    def __init__(self, player_0, player_1, seed, wait, log_file):
+        if seed is None:
+            seed = Random().randrange(1, 1_000)
+
+        print(f"seed: {seed}", file=log_file)
+
         self.players = {
             "player-0": player_0,
             "player-1": player_1
         }
         self.seed = seed
         self.wait = wait
+        self.log_file = log_file
 
     async def terminate(self):
         for player in self.players.values():
@@ -76,7 +83,10 @@ class Game:
             actions = {}
             for name, player in self.players.items():
                 try:
-                    actions[name] = await player.get_action(observations[name])
+                    action = await player.get_action(observations[name])
+                    print(f"{name}: {action}", file=self.log_file)
+
+                    actions[name] = action
 
                 except asyncio.TimeoutError:
                     print(f"{name}: time out...", file=sys.stderr)
@@ -84,8 +94,9 @@ class Game:
                     await self.terminate()
                     return dict(map(lambda key: (key, 1.0 if key != name else -1.0), self.players.keys()))
 
-                except Exception:
-                    print(f"{name}: something wring...")
+                except Exception as e:
+                    print(f"{name}: something wrong...", file=sys.stderr)
+                    print(e, file=sys.stderr)
 
                     await self.terminate()
                     return dict(map(lambda key: (key, 1.0 if key != name else -1.0), self.players.keys()))
@@ -110,12 +121,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     async def main(player_0_command, player_1_command, seed, wait):
-        result = await Game(
-            await create_player(player_0_command, open("./player-0.log", mode="w")),
-            await create_player(player_1_command, open("./player-1.log", mode="w")),
-            seed,
-            wait
-        ).play()
+        with open("./game.log", mode="w") as log_file:
+            result = await Game(
+                await create_player(player_0_command, open("./player-0.log", mode="w")),
+                await create_player(player_1_command, open("./player-1.log", mode="w")),
+                seed,
+                wait,
+                log_file
+            ).play()
 
         print(f"{result['player-0']}\t{result['player-1']}")
 
---
+++ game/replay.py
@@ -0,0 +1,62 @@
+import asyncio
+import json
+import numpy as np
+import os
+import sys
+
+from argparse import ArgumentParser
+from funcy import first
+from physical_quoridor import PhysicalQuoridorEnv
+from random import Random
+from time import sleep, perf_counter
+from play import Game, Player
+
+
+class LogPlayer(Player):
+    def __init__(self, actions):
+        self.actions_it = iter(actions)
+
+    async def get_action(self, observation):
+        return next(self.actions_it)
+
+    async def end_game(self):
+        pass
+
+
+async def main():
+    with open("./game.log") as log_file:
+        log_lines = list(map(lambda line: line.strip(), log_file.readlines()))
+
+    seed = int(first(filter(
+        lambda log_line: log_line.startswith("seed:"),
+        log_lines
+    )).split(":")[1])
+
+    player_0_actions = list(map(
+        lambda action_line: eval(action_line.split(":")[1]),
+        filter(
+            lambda log_line: log_line.startswith("player-0:"),
+            log_lines
+        )
+    ))
+
+    player_1_actions = list(map(
+        lambda action_line: eval(action_line.split(":")[1]),
+        filter(
+            lambda log_line: log_line.startswith("player-1:"),
+            log_lines
+        )
+    ))
+
+    with open(os.devnull, mode="w") as log_file:
+        result = await Game(
+            LogPlayer(player_0_actions),
+            LogPlayer(player_1_actions),
+            seed,
+            True,
+            log_file
+        ).play()
+
+    print(f"{result['player-0']}\t{result['player-1']}")
+
+asyncio.run(main())
