0c94fa75e259dadd49596c57b8bba2c39ee58fa6
Ojima Ryoji <ryoji.ojima@biprogy.com>
2025-04-22 14:17:47 +0900

    ビルドの設定を少し変更。

--- physical-quoridor/_t.ps1
+++ physical-quoridor/_t.ps1
@@ -2,4 +2,4 @@ $Env:RUSTFLAGS=""
 maturin develop --release --features python  # && python .\_t.py
 
 $Env:RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
-wasm-pack build --target bundler --release --features javascript
+wasm-pack build --target bundler --no-typescript --release --features javascript
