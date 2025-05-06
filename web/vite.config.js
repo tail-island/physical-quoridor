50e88ac7b56dd2c17dd06d2e1f22cab8145b34a1
Ojima Ryoji <ryoji.ojima@biprogy.com>
2025-05-06 18:16:03 +0900

    とりあえず、一通り動いた！

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import wasm from "vite-plugin-wasm";
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,6 +23,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
